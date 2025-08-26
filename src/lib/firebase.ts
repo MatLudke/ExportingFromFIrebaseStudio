b4626c3d351f700d729609f9522877e3c39b246e
William Ludke <mateus1ludke@gmail.com>
2025-08-26 16:55:04 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "tempo-certo-qf64u",
+  "appId": "1:1044295739285:web:d583b8dfdb56de33ec789c",
+  "storageBucket": "tempo-certo-qf64u.firebasestorage.app",
+  "apiKey": "AIzaSyBCuaWiC3-tlpttDR_PBfGgKtFsKMflrK8",
+  "authDomain": "tempo-certo-qf64u.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1044295739285"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
